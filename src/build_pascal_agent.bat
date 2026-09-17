c2888053dadf68673996e97d6401a951ce06270b
老张 <passbyyou888@outlook.com>
2026-09-17 16:25:43 +0800

    Update build_pascal_agent.bat

--- src/build_pascal_agent.bat
+++ src/build_pascal_agent.bat
@@ -1,6 +1,7 @@
 lazbuild.exe -B ./pascal_agent_service.lpi
 lazbuild.exe -B ./pascal_agent_api.lpi
 lazbuild.exe -B ./CreateHealthCheck/HealthCheck.lpi
+lazbuild.exe -B ./llm_tool_v3.lpi
 
 echo 所有项目编译完成。
 timeout /t 5 /nobreak >nul
